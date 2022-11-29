e7d5502655d7266cfec3217f092a5aae11931675
spiritifilled0 <israelprinced@gmail.com>
2022-11-29 07:35:19 +0100

    added first java project T14

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is my first java project");
+        System.out.println("As a software tester");
+        System.out.println("In the tech world");
+
+    }
+}
